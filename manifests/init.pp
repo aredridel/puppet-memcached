1f999c621c26a96aa17ba28b0a70a5d1128473e1
Drew Blessing <Drew.Blessing@Buckle.com>
2013-02-02 21:06:31 -0600

    Change params

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,5 @@
 class memcached(
   $package_ensure           = 'present',
-  $default_instance_ensure  = 'present',
   $logfile                  = '/var/log/memcached.log',
   $max_memory               = false,
   $lock_memory              = false,
@@ -10,24 +9,27 @@ class memcached(
   $user                     = $::memcached::params::user,
   $max_connections          = '8192',
   $verbosity                = undef,
-  $unix_socket              = undef
+  $unix_socket              = undef,
+  $install_default_instance = true,
 ) inherits memcached::params {
 
   package { $memcached::params::package_name:
     ensure => $package_ensure,
   }
 
-  memcached::instance { "default":
-    instance_ensure => 'present',
-    logfile         => $logfile,
-    max_memory      => $max_memory,
-    lock_memory     => $lock_memory,
-    listen_ip       => $listen_ip,
-    tcp_port        => $tcp_port,
-    udp_port        => $udp_port,
-    user            => $user,
-    max_connections => $max_connections,
-    verbosity       => $verbosity,
-    unix_socket     => $unix_socket,
+  if ($install_default_instance == true) {
+    memcached::instance { "default":
+      instance_ensure => 'present',
+      logfile         => $logfile,
+      max_memory      => $max_memory,
+      lock_memory     => $lock_memory,
+      listen_ip       => $listen_ip,
+      tcp_port        => $tcp_port,
+      udp_port        => $udp_port,
+      user            => $user,
+      max_connections => $max_connections,
+      verbosity       => $verbosity,
+      unix_socket     => $unix_socket,
+    }
   }
 }
